0f558ba4991c181950e99d74fb7ec3f57cc5109a
supGambler <artur.agababian2020@gmail.com>
2021-08-17 02:39:40 -0700

    fix notification error

--- XFStructure/XFStructure.Android/MainApplication.cs
+++ XFStructure/XFStructure.Android/MainApplication.cs
@@ -19,32 +19,32 @@ namespace XFStructure.Droid
             
             base.OnCreate();
 
-//            try
-//            {
-//                FirebaseApp.InitializeApp(this);
+            try
+            {
+                FirebaseApp.InitializeApp(this);
 
 //                //Set the default notification channel for your app when running Android Oreo
-//                if (Build.VERSION.SdkInt >= BuildVersionCodes.O)
-//                {
+                if (Build.VERSION.SdkInt >= BuildVersionCodes.O)
+                {
 //                    //Change for your default notification channel id here
-//                    FirebasePushNotificationManager.DefaultNotificationChannelId = "default";
+                    FirebasePushNotificationManager.DefaultNotificationChannelId = "default";
 
 //                    //Change for your default notification channel name here
-//                    FirebasePushNotificationManager.DefaultNotificationChannelName = "default";
-//                }
+                    FirebasePushNotificationManager.DefaultNotificationChannelName = "default";
+                }
 
 
 //                //If debug you should reset the token each time.
 //#if DEBUG
 //                FirebasePushNotificationManager.Initialize(this, true);
 //#else
-//            FirebasePushNotificationManager.Initialize(this,false);
+                FirebasePushNotificationManager.Initialize(this,false);
 //#endif
-//            }
-//            catch(Exception ex)
-//            {
+            }
+            catch(Exception ex)
+            {
 
-//            }
+            }
 
             //Handle notification when app is closed here
             //CrossFirebasePushNotification.Current.OnNotificationReceived += (s, p) =>
@@ -53,14 +53,14 @@ namespace XFStructure.Droid
 
             //};
             //Set the default notification channel for your app when running Android Oreo
-            //if (Build.VERSION.SdkInt >= Android.OS.BuildVersionCodes.O)
-            //{
+            if (Build.VERSION.SdkInt >= Android.OS.BuildVersionCodes.O)
+            {
             //    //Change for your default notification channel id here
-            //    FirebasePushNotificationManager.DefaultNotificationChannelId = "DefaultChannel";
+                FirebasePushNotificationManager.DefaultNotificationChannelId = "DefaultChannel";
 
             //    //Change for your default notification channel name here
-            //    FirebasePushNotificationManager.DefaultNotificationChannelName = "General";
-            //}
+                FirebasePushNotificationManager.DefaultNotificationChannelName = "General";
+            }
 
 
             //If debug you should reset the token each time.
@@ -79,28 +79,24 @@ namespace XFStructure.Droid
 
             //            }, true);
             //#else
-            //	            FirebasePushNotificationManager.Initialize(this,new NotificationUserCategory[]
-            //		    {
-            //			new NotificationUserCategory("message",new List<NotificationUserAction> {
-            //			    new NotificationUserAction("Reply","Reply",NotificationActionType.Foreground),
-            //			    new NotificationUserAction("Forward","Forward",NotificationActionType.Foreground)
+            FirebasePushNotificationManager.Initialize(this,new NotificationUserCategory[]
+            {
+                new NotificationUserCategory("message",new List<NotificationUserAction> {
+                    new NotificationUserAction("Reply","Reply",NotificationActionType.Foreground),
+                    new NotificationUserAction("Forward","Forward",NotificationActionType.Foreground)
 
-            //			}),
-            //			new NotificationUserCategory("request",new List<NotificationUserAction> {
-            //			    new NotificationUserAction("Accept","Accept",NotificationActionType.Default,"check"),
-            //			    new NotificationUserAction("Reject","Reject",NotificationActionType.Default,"cancel")
-            //			})
-
-            //		    },false);
+                }),
+                new NotificationUserCategory("request",new List<NotificationUserAction> {
+                    new NotificationUserAction("Accept","Accept",NotificationActionType.Default,"check"),
+                    new NotificationUserAction("Reject","Reject",NotificationActionType.Default,"cancel")
+                })
+       	    },false);
             //#endif
 
             //CrossFirebasePushNotification.Current.OnNotificationReceived += (s, p) =>
             //{
             //    System.Diagnostics.Debug.WriteLine("NOTIFICATION RECEIVED", p.Data);
             //};
-
-
-
         }
     }
 }
